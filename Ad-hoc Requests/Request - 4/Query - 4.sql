6368e71270fa9300e0162855ee9f279fba4b3369
Priya Jangid <jangiddpriya@gmail.com>
2024-12-31 21:13:15 +0530

    Update Query - 4.sql

--- Ad-hoc Requests/Request - 4/Query - 4.sql
+++ Ad-hoc Requests/Request - 4/Query - 4.sql
@@ -1,37 +1,27 @@
-WITH RankedCities AS (
-    SELECT 
-        dc.city_name,
-        SUM(ps.new_passengers) AS total_new_passengers,
-        RANK() OVER (ORDER BY SUM(ps.new_passengers) DESC) AS rank_desc,
-        RANK() OVER (ORDER BY SUM(ps.new_passengers) ASC) AS rank_asc
-    FROM 
-        fact_passenger_summary ps
-    JOIN 
-        dim_city dc
-    ON 
-        ps.city_id = dc.city_id
-    GROUP BY 
-        dc.city_name
+WITH 
+cityRank AS (
+     SELECT dc.city_name,
+            SUM(ps.new_passengers) AS total_new_passengers,
+            DENSE_RANK() OVER (ORDER BY SUM(ps.new_passengers) ASC) AS rank_asc,
+            DENSE_RANK() OVER (ORDER BY SUM(ps.new_passengers) DESC) AS rank_desc
+     FROM dim_city dc
+     JOIN fact_passenger_summary ps
+     ON dc.city_id = ps.city_id
+     GROUP BY dc.city_id
 ),
-CategorisedCities AS (
-    SELECT 
-        city_name,
-        total_new_passengers,
-        CASE 
-            WHEN rank_desc <= 3 THEN 'Top 3'
-            WHEN rank_asc <= 3 THEN 'Bottom 3'
-            ELSE NULL
-        END AS city_category
-    FROM 
-        RankedCities
+categrisedCity AS (
+	SELECT 
+      city_name,
+      total_new_passengers,
+    CASE 
+    WHEN rank_desc <= 3 THEN "Top 3"
+    WHEN rank_asc <= 3 THEN "Bottom 3" ELSE NULL END AS city_category
+    FROM cityRank
 )
-SELECT 
-    city_name,
-    total_new_passengers,
+SELECT city_name,
+	total_new_passengers,
     city_category
-FROM 
-    CategorisedCities
-WHERE 
-    city_category IS NOT NULL
-ORDER BY 
-    total_new_passengers DESC;
+FROM categrisedCity
+WHERE city_category IS NOT NULL
+GROUP BY city_name
+ORDER BY total_new_passengers DESC;
